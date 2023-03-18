0ec994d43b873a03c3fcc8e426d8fc9334101c21
Małgorzata Nikiel <nikielmalgorzata@gmail.com>
2023-03-18 22:14:11 +0100

    feat: add styles for main layout

--- src/app/components/shared/layout/Layout.tsx
+++ src/app/components/shared/layout/Layout.tsx
@@ -1,8 +1,14 @@
+import { useTranslation } from 'react-i18next';
 import { Outlet } from 'react-router-dom';
 
 const Layout = () => {
+  const { t } = useTranslation();
+
   return (
-    <main>
+    <main className="grid grid-rows-[var(--topbar-height)_auto]">
+      <header className="bg-pink-700 p-4">
+        <h1 className="text-xl text-white">{t('translation:common.title')}</h1>
+      </header>
       <Outlet />
     </main>
   );
