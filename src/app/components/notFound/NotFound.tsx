b76312ae77a62c05215f05e018e2706a0bfa188d
Małgorzata Nikiel <nikielmalgorzata@gmail.com>
2023-03-18 22:37:59 +0100

    feat: adjust styles of NotFound page

--- src/app/components/notFound/NotFound.tsx
+++ src/app/components/notFound/NotFound.tsx
@@ -1,3 +1,4 @@
+import { Button } from '@app/components/shared/button/Button';
 import { ROUTES } from '@app/shared/routes';
 import { useTranslation } from 'react-i18next';
 import { Link } from 'react-router-dom';
@@ -6,11 +7,11 @@ const NotFound = () => {
   const { t } = useTranslation();
 
   return (
-    <main>
-      <h1>{t('translation:not-found:title')}</h1>
-      <p>{t('translation:not-found:explanation-message')}</p>
+    <main className="flex h-[100vh] w-full flex-col items-center justify-center gap-2">
+      <h1 className="text-4xl text-pink-700">{t('translation:not-found:title')}</h1>
+      <p className="text-base text-pink-700">{t('translation:not-found:explanation-message')}</p>
       <Link to={ROUTES.superheroes}>
-        <button className="capitalize">{t('translation:not-found:go-home')}</button>
+        <Button className="uppercase">{t('translation:not-found:go-home')}</Button>
       </Link>
     </main>
   );
